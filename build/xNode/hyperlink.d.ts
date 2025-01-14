0b789366ac4eadc3ceb84cec18c3ec608d71ee28
ShenXiaowei <shenxiaowei@growingio.com>
2025-01-14 17:36:02 +0800

    feat: 2.0.14

--- build/xNode/hyperlink.d.ts
+++ build/xNode/hyperlink.d.ts
@@ -1,2 +1,2 @@
 import { Possible } from '@/typings';
-export declare const getHyperlink: (node: Element, tagName: string) => Possible<string>;
+export declare const getHyperlink: (node: Element, tagName: string) => Possible<string[]>;
